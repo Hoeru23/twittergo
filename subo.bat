f44be0b01b357479ef87ac6f9b7ea8720f154beb
Joel Matos <joel_matos@bhdleon.com.do>
2024-11-04 10:28:26 -0400

    Cambio a bootstrap

--- subo.bat
+++ subo.bat
@@ -1,6 +1,6 @@
 git add .
 git commit -m "Cambio a bootstrap"
 git push
-GOARCH=amd64 GOOS=linux go build -o bootstrap main.go
+$Env:GOARCH=arm64; $Env:GOOS=linux; go build -o bootstrap main.go
 del main.zip
 tar.exe -a -cf main.zip bootstrap
